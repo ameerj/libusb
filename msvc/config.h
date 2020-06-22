71c3083fdd07a5a8c30a469800563bfbd9f9d4ad
Ameer <aj662@drexel.edu>
2020-06-21 22:05:19 -0400

    fix #warn preproccessor directive

--- msvc/config.h
+++ msvc/config.h
@@ -1,7 +1,7 @@
 /* config.h.  Manual config for MSVC.  */
 
 #ifndef _MSC_VER
-#warn "msvc/config.h shouldn't be included for your development environment."
+#warning "msvc/config.h shouldn't be included for your development environment."
 #error "Please make sure the msvc/ directory is removed from your build path."
 #endif
 
@@ -49,3 +49,4 @@
 #define OS_WINDOWS 1
 #define HAVE_SYS_TYPES_H 1
 #endif
+
